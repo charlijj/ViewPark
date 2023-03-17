eb45e3de8c69972ce0a4cdc82e60b3c8944df54d
charlijj <jjcharlinski@gmail.com>
2023-03-16 17:24:41 -0700

    added availability css and html

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
     include_once("php/head.php");
 ?>
 
+<div class="header">
+    <h1>View Park</h1>
+    <h3>Get the most up to date and accurate VIU parking information (slogan needs work)</h3>
+</div>
+
+<?php
+    include_once("php/availability.php");
+?>
+
 <?php
     include_once("php/tail.php");
 ?>
---
+++ php/availability.php
@@ -0,0 +1,10 @@
+<div class="availability-container">
+    <div class="availability-container-item">Parking Lot 1</div>
+    <div class="availability-container-item">Parking Lot 2</div>
+    <div class="availability-container-item">Parking Lot 3</div>
+    <div class="availability-container-item">Parking Lot 4</div>
+    <div class="availability-container-item">Parking Lot 5</div>
+    <div class="availability-container-item">Parking Lot 6</div>
+    <div class="availability-container-item">Parking Lot 7</div>
+    <div class="availability-container-item">Parking Lot 8</div>
+</div>
