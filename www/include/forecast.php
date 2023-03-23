055e1f36c347e7ff0b0d19909a5da4f68e2c9754
Adam Newman <adam@Adam-MBP.local>
2023-03-23 15:56:28 -0700

    Add test forecast graph

--- www/include/forecast.php
+++ www/include/forecast.php
@@ -3,8 +3,33 @@
 
 ?>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const ctx = document.getElementById('forecast-graph');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+</script>
+
 <div class="forecast-container">
-    <div class="forecast-graph">
-        <p>Forecast<p>
+    <div class="forecast-graph-container">
+        <canvas id="forecast-graph"></canvas>
     </div>
 </div>
