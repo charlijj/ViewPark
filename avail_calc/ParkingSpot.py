5b8ac1095438e4fc497c5c3a76deb4aef70da0a3
haymm <mhay302@gmail.com>
2023-03-23 23:28:10 -0700

    found out what dataclasses are

--- avail_calc/ParkingSpot.py
+++ avail_calc/ParkingSpot.py
@@ -1,12 +1,11 @@
+from dataclasses import dataclass
 # ParkingSpot Class
 # This file contains a class which descibes a parking spot as a rectangle in space.
 
+@dataclass
 class ParkingSpot:
-    
     # note: x and y positions describe the point of the top left corner of the rectangle
-    def __init__(self, xPos, yPos, spotWidth, spotHeight):
-        self.xPos = xPos
-        self.yPos = yPos
-        self.spotWidth = spotWidth
-        self.spotHeight = spotHeight
-        
+    xPos: int
+    yPos: int
+    spotWidth: int
+    spotHeight: int
