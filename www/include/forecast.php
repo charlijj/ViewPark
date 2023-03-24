0455e9c2fac33378d1459c6406e4eda8437fc4f8
Adam Newman <adam@Adam-MBP.local>
2023-03-23 17:33:27 -0700

    Revert adding chart

--- www/include/forecast.php
+++ www/include/forecast.php
@@ -5,35 +5,38 @@
 
 <div class="forecast-container">
     <div class="forecast-graph-container">
-        <canvas id="forecast-graph"></canvas>
+        <!-- <canvas id="forecast-graph"></canvas> -->
+        Forecast Graph
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js">
+<script></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
     const ctx = document.getElementById('forecast-graph');
 
     new Chart(ctx, {
         type: 'bar',
         data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
         },
         options: {
-        scales: {
-            y: {
-            beginAtZero: true
+            scales: {
+                y: {
+                beginAtZero: true
+                }
             }
         }
-        }
     });
-</script>
+</script> -->
 
-<script>
+<!-- <script>
     // window.localStorage.getItem('lotId');
 
-</script>
+</script> -->
 
