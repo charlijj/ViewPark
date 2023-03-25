98f1b3ce363ab7e7c86866e09dc97cc507ee5bd7
Adam Newman <adam@w134-87-033-057.wireless.uvic.ca>
2023-03-25 16:36:00 -0700

    Add day selection

--- www/include/forecast.php
+++ www/include/forecast.php
@@ -1,42 +1,39 @@
 <?php
 
+    include_once("database.php");
+
+    $db = new Database;
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // If the form was submitted, get the selected day of the week from the POST data
+        $selected_day = $_POST['forecast-day'];
+      
+        // Set the $forecast_day variable to the selected day
+        $forecast_day = $selected_day;
+    } else {
+        // If the form was not submitted, set the $forecast_day variable to the current day
+        $forecast_day = date('l'); // 'l' format returns the full name of the day of the week (e.g. "Monday")
+    }
 
 ?>
 
 <div class="forecast-container">
-    Forecast Graph
-    <div>
+    <div class="forecast-title">
+        <?php echo $forecast_day ?> Forecast
+    </div>
+    <div class="forecast-graph-container">
         <canvas id="forecast-graph"></canvas>
     </div>
+    <div class="day-select">
+        <label for="forecast-day">View forecast for:</label>
+            <select id="forecast-day" name="forecast-day">
+                <option value="monday">Monday</option>
+                <option value="tuesday">Tuesday</option>
+                <option value="wednesday">Wednesday</option>
+                <option value="thursday">Thursday</option>
+                <option value="friday">Friday</option>
+                <option value="saturday">Saturday</option>
+                <option value="sunday">Sunday</option>
+            </select>
+    </div>
 </div>
-
-
-<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
-    const ctx = document.getElementById('forecast-graph');
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                beginAtZero: true
-                }
-            }
-        }
-    });
-</script> -->
-
-<!-- <script>
-    // window.localStorage.getItem('lotId');
-
-</script> -->
-
