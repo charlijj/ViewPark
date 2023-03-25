19332c49350f1fbc6def956013e0598b97ba2533
Adam Newman <adam@w134-87-033-057.wireless.uvic.ca>
2023-03-25 15:47:45 -0700

    Move forecast script to tail

--- www/include/tail.php
+++ www/include/tail.php
@@ -2,5 +2,8 @@
         <p>View Park Mar 12th, 2023 &copy;</p>
         <p>View <a href="https://github.com/charlijj/VIU-Parking-Lot-Occupancy-Tracker" target="_blank">source code</a></p>
     </footer>
+    <!-- chart.js library for forecast charts -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="forecast.js"></script>
 </body>
 </html>
