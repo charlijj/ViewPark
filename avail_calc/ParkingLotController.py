fd9ac36a8db5be038bf21870b90692395dc42c63
David <dboterov@hotmail.com>
2023-03-27 12:43:59 -0700

    Trying to instantiate ParkingLot class to make it do a calc on 'lotA1'.

--- avail_calc/ParkingLotController.py
+++ avail_calc/ParkingLotController.py
@@ -1,35 +1,57 @@
 import cv2 as cv
 
+# has (self, lotID, lotName, lotType, posListFileName, parkingLotImage)
+from ParkingLot import ParkingLot 
+
 # importing sql/databse.py to query/insert/update the databse
 import sys
 sys.path.append('../sql')
-from database import Database
-"""
-need to make file path dynamic?
-or maybe '../sql' works fine everywhere?
-"""
-from ParkingLot import ParkingLot # has (self, lotID, lotName, lotType, posListFileName, parkingLotImage)
+from sql.database import Database, LotEntry
+#generate_select_query, generate_insert_query, generate_update_query
 
-my_instance = Database #trying to instantaite the databse
+db = Database #trying to instantaite the databse
 
-# create the select query to grab the lot info
+
+# create the select query to grab the lot A info
+lot_entry = LotEntry(lotId=2)
+pos_list_file_name = './img/lotA/posList'
+parking_lot_imageA1 = './img/lotA/lotA1.jpg'
+
+parking_lot = ParkingLot(lot_entry, pos_list_file_name, parking_lot_imageA1)
+avail = ParkingLot.updateFullness(parking_lot)
+print(parking_lot) #prints "fullness=none"
+
+
+
+
+ 
+"""
+lot_entry = LotEntry()
+lot_entry.lot_name = "lot A"
+"""
+
+
+
+
+
+"""
 query = "lot"
-args = ["lotId", "lotName"]
+args = ["lotName", "Lot A", ]
 select_query = my_instance.generate_select_query(query, args)
+"""
 
 # then pass the lot info to the ParkingLot class
+
+
+
+
+
+
 # call that classes method and store the update availabilty in a variable here
 
 # Then, using the methods from database.py:
 # UPDATE fullness in the DB table that just need and UPDATE.
 # and INSERT fullness in the other table that creates a row every 5 mins.
-
-
-
-
-
-
-
 
 
 """ DONT MIND THIS, JUST TRYING TO MAKE SENSE OF IT
