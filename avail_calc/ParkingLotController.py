17136c0cca5f83026607886fd411b70747f3eb8b
David <dboterov@hotmail.com>
2023-03-27 17:08:53 -0700

    Renamed all images to have lower case 'l'. And minor changes to ParkingLotController. Working on the itrator for every lot image.

--- avail_calc/ParkingLotController.py
+++ avail_calc/ParkingLotController.py
@@ -14,43 +14,41 @@ from sql.database import Database, LotEntry
 db = Database #trying to instantaite the databse
 
 # grab lot A info
-lot_entryA = LotEntry(lotId=2)
-pos_list_file_nameA = './img/lotA/posList' #use this value for all 'lotA' images
+
+#pos_list_file_nameA = './img/lotA/posList' #use this value for all 'lotA' images
 
 # define the list of lotA images to loop through
-lotA_images = cycle(['./img/lotA/lotA1.jpg', './img/lotA/lotA2.jpg', './img/lotA/lotA3.jpg'])
+#lotA_images = cycle(['./img/lotA/lotA1.jpg', './img/lotA/lotA2.jpg', './img/lotA/lotA3.jpg'])
 
 #parking_lot_imageA1 = './img/lotA/lotA1.jpg'
 
 # calculate lot A avail and return it to here
 while True:
+    for i in range(1, 6): #loop thru the 5 lotx1's 
+        lot_id = i + 1 #lotA1 has lot_id 2, lotB1 has lot_id 3, lotC1 has lot_id 4
+        lot_entry = LotEntry(lotId=lot_id) # starts at lot_Id = 2
+        name = chr(64+i)
+        pos_list_file_name = f'./img/lot{name}/posList' # value for all 'lotA' images
+        lot_images = [f'./img/lot{name}/lot{name}{j}.jpg' for j in range(1, 4)] # get all 3 images for this lot
 
-    # iterate throug each image in a circular way
-    parking_lot_imageA = next(lotA_images)
+        avail_list = []
+        for image in lot_images:
+            parking_lot = ParkingLot(lot_entry, pos_list_file_name, image)
+            avail_lot = parking_lot.updateFullness()
 
-    parking_lotA = ParkingLot(lot_entryA, pos_list_file_nameA, parking_lot_imageA)
-    avail_lotA = parking_lotA.updateFullness()
 
     # need to update and insert to database instead of printing
-    print(avail_lotA)
+        print(f'Lot {chr(64+i)}:')
+        for j, avail in enumerate(avail_list):
+                print(f'\tImage {j+1}: {avail}')
 
-    # waits 5 min before processing next image
-    time.sleep(300)
+    # waits 5 sec before processing next image. Change to 5 min after testing 
+    time.sleep(5)
 
-
-
-
-
-
- 
 """
 lot_entry = LotEntry()
 lot_entry.lot_name = "lot A"
 """
-
-
-
-
 
 """
 query = "lot"
@@ -59,12 +57,6 @@ select_query = my_instance.generate_select_query(query, args)
 """
 
 # then pass the lot info to the ParkingLot class
-
-
-
-
-
-
 # call that classes method and store the update availabilty in a variable here
 
 # Then, using the methods from database.py:
@@ -87,7 +79,7 @@ fullness = PL.updateFullness()
 #query the database for parking lot info
 
 
-#print("Good to here maybe")
+print("Good to here maybe")
 
 
 
