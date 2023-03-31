1759fb01d0f730ef077410428d276cd0c4e47e0d
Ethan Bryson <brysone@stumail.csci.viu.ca>
2023-03-30 18:55:39 -0700

    Fixed import for package

--- avail_calc/__init__.py
+++ avail_calc/__init__.py
@@ -1,5 +1,7 @@
-from .Availability import *
-from .ParkingLot import *
-from .ParkingSpot import *
-from .ParkingLotController import *
+import os, sys
+sys.path.append(os.path.dirname(__file__))
 
+from Availability import *
+from ParkingLot import *
+from ParkingSpot import *
+from ParkingLotController import *
